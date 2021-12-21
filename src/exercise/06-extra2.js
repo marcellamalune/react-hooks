0ed3917d7a6a602b965eef0fb01f2c9b8812c946
Marcella Malune <marcella.malune@hrmgroup.it>
2021-12-21 11:15:08 +0100

    es.6 pokemon extra 2

--- src/exercise/06-extra2.js
+++ src/exercise/06-extra2.js
@@ -2,46 +2,30 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
 import {PokemonForm, fetchPokemon, PokemonInfoFallback, PokemonDataView} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
-  // 🐨 Have state for the pokemon (null)
   const [pokemon, setPokemon] = React.useState(null)
   const [error, setError] = React.useState(null)
-  const [state, setState] = React.useState('idle')
-  // 🐨 use React.useEffect where the callback should be called whenever the
-  // pokemon name changes.
-  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
-  // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
+  const [status, setStatus] = React.useState('idle')
+
   React.useEffect(() => {
     if (!pokemonName) return null
     setPokemon(null)
-    setState('pending')
+    setStatus('pending')
     fetchPokemon(pokemonName)
         .then( pokemonData => {
             /* update all the state here */
             setPokemon(pokemonData)
-            setState('resolved')
+            setStatus('resolved')
         })
         .catch( error => {
             setError(error)
-            setState('rejected')
+            setStatus('rejected')
         })
   }, [pokemonName])
-  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null
-  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-  //   fetchPokemon('Pikachu').then(
-  //     pokemonData => {/* update all the state here */},
-  //   )
-  // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
-  //   1. no pokemonName: 'Submit a pokemon'
-  //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
-  switch (state) {
+
+  switch (status) {
     case 'pending':
         return <PokemonInfoFallback name={pokemonName} />
     case 'resolved':
@@ -55,9 +39,6 @@ function PokemonInfo({pokemonName}) {
     default:
         return 'Submit a pokemon'
   }
-
-  // 💣 remove this
-  //return 'TODO'
 }
 
 function App() {
